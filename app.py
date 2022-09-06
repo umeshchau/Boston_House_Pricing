4e5eac91ee8084d5083d29879e7abb74ba1f7051
Umesh Chauhan <umeshuc.94@gmail.com>
2022-09-06 14:40:04 +0530

    The ewb app is ready

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import json
 import pickle
+
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
 ## Load the model
-regmodel=pickle.load(open('reg_model.pkl','rb'))
-scaler=pickle.load(open('scaling.pkl','rb'))
-
+reg_model=pickle.load(open('reg_model.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -18,10 +18,20 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=reg_model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=reg_model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The Boston House Price Prediction is {}".format(output))
+
+
+
 if __name__=="__main__":
-    app.run(debug=True)    
+    app.run(debug=True) 
